47f3f75a94b670f3398e4055fc972ebbca041df4
Bachhsunkid <86517817+Bachhsunkid@users.noreply.github.com>
2023-04-10 16:41:11 +0700

    Before handle cart

--- ShoesStore/Controllers/HomeAPIController.cs
+++ ShoesStore/Controllers/HomeAPIController.cs
@@ -12,38 +12,6 @@ namespace ShoesStore.Controllers
     {
         Qlbangiaynhom7Context db = new Qlbangiaynhom7Context();
 
-        [HttpGet("GetShoeByFilter")]
-        public IEnumerable<GiayDTO> GetShoeByFilter(int page = 1)
-        {
-            int pageNumber = page;
-            int pageSize = 3;
-
-            var lstGiay = db.Giays.GroupBy(x => x.TenGiay)
-                        .Select(group => group.First())
-                        .ToList();
-
-            List<GiayDTO> lstShoesFiltered = new List<GiayDTO>();
-
-            foreach (var item in lstGiay)
-            {
-                GiayDTO temp = new GiayDTO
-                {
-                    MaGiay = item.MaGiay,
-                    TenLoai = db.LoaiGiays.Find(item.MaLoai).TenLoai,
-                    TenGiay = item.TenGiay,
-                    KichCo = item.KichCo,
-                    MauSac = item.MauSac,
-                    GiaGoc = item.GiaGoc,
-                    GiaBan = item.GiaBan,
-                    PhanTramGiam = item.PhanTramGiam,
-                    DanhGia = item.DanhGia,
-                    AnhDaiDien = item.AnhDaiDien,
-                    SoLuong = item.SoLuong
-                };
-                lstShoesFiltered.Add(temp);
-            }
-
-            return lstShoesFiltered;
-        }
+        
     }
 }
